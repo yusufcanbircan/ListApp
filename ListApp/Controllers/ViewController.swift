75b3a8f2ef6d9440767208600c609c9a23de8436
yusufcanbircan <yusufcannnz@hotmail.com>
2022-07-23 16:33:32 +0300

    Initial Commit

---
+++ ListApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListApp
+//
+//  Created by Yusuf Can Bircan on 23.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
